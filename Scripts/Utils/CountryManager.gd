5e44668a15907dbba84544504aca131a2efe8fdb
Tenseven-107 <78176691+Tenseven-107@users.noreply.github.com>
2023-05-03 03:37:30 +0200

    Added a risk system

--- Scripts/Utils/CountryManager.gd
+++ Scripts/Utils/CountryManager.gd
@@ -6,7 +6,12 @@ export (NodePath) var country_container_path = null
 var country_container = null
 
 var spawn_timer: Timer
-export (float) var spawn_time: float = 7.5
+var current_spawn_time: float
+export (float) var max_spawn_time: float = 20
+export (float) var min_spawn_time: float = 5
+
+var risk: float = 0
+export (float) var risk_increase: float = 0.001
 
 
 # Set up
@@ -20,7 +25,8 @@ func _ready():
 func instance_timer():
 	spawn_timer = Timer.new()
 
-	spawn_timer.wait_time = spawn_time
+	current_spawn_time = max_spawn_time
+	spawn_timer.wait_time = current_spawn_time
 	spawn_timer.one_shot = true
 	spawn_timer.connect("timeout", self, "spawn_timeout")
 
@@ -31,5 +37,25 @@ func instance_timer():
 
 # Spawn a new plane after cooldown
 func spawn_timeout():
+	increase_risk()
+
 	spawn_timer.start()
 	country_container.spawn_plane()
+
+
+
+# Increase risk level
+func increase_risk():
+	risk += risk_increase
+
+	if current_spawn_time > min_spawn_time:
+		current_spawn_time -= risk
+		current_spawn_time = clamp(current_spawn_time, min_spawn_time, max_spawn_time)
+
+		spawn_timer.wait_time = current_spawn_time
+
+
+
+
+
+
